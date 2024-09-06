c871d9b2dcaafb6d5b17d207270eff2c644fb35a
Henry Blanchette <henry@monkeyface.com>
2024-09-06 14:40:44 -0400

    checkpoint

--- src/Sexpze/Component/Editor.purs
+++ src/Sexpze/Component/Editor.purs
@@ -2,16 +2,26 @@ module Sexpze.Component.Editor where
 
 import Prelude
 
+import Control.Monad.State (get, modify_)
 import Control.Plus (empty)
-import Data.Maybe (Maybe(..))
+import Data.Array as Array
+import Data.List (List)
+import Data.List as List
+import Data.Maybe (Maybe(..), maybe)
 import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Sexpze.Data.Sexp (Sexp(..))
-import Sexpze.Data.Sexp.Cursor (Cursor(..), topPoint)
+import Sexpze.Data.Sexp.Cursor (Cursor(..), Point(..), Span(..), moveLeft_Cursor, moveRight_Cursor, topPoint)
 import Sexpze.Utility (todo)
+import Web.Event.Event as Event
 import Web.UIEvent.KeyboardEvent (KeyboardEvent)
 import Web.UIEvent.KeyboardEvent as KeyboardEvent
+import Web.UIEvent.MouseEvent (MouseEvent)
+import Web.UIEvent.MouseEvent as MouseEvent
 
 --------------------------------------------------------------------------------
 -- component
@@ -28,6 +38,8 @@ type Slots = ()
 
 type M = Aff
 
+type HTML = H.ComponentHTML Action Slots M
+
 component :: H.Component Query Input Output M
 component = H.mkComponent { initialState, eval, render }
   where
@@ -41,11 +53,51 @@ component = H.mkComponent { initialState, eval, render }
 
   handleAction = case _ of
     UserAction_Action a -> handleUserAction a
+    SetCursor_Action c mb_event -> do
+      mb_event # maybe (pure unit) (Event.stopPropagation <<< MouseEvent.toEvent) # liftEffect
+      modify_ _ { cursor = c }
 
   render _state =
     HH.div
       []
       [ HH.text "Editor.component" ]
+
+renderTerm :: List Int -> Term -> HTML
+renderTerm is (Atom label) =
+  HH.div [ HP.classes [ H.ClassName "sexp", H.ClassName "atom" ] ]
+    [ HH.text label ]
+renderTerm is (Group xs) =
+  let
+    p0 = Point is 0
+    p1 = Point is (Array.length xs)
+  in
+    HH.div [ HP.classes [ H.ClassName "sexp", H.ClassName "group" ] ]
+      ( [ [ renderSpanHandle (Span { p0, p1 }) (HH.text "(") ]
+        , xs
+            # Array.mapWithIndex
+                ( \j x ->
+                    [ renderPointHandle (Point is j) (HH.text "•")
+                    , x # renderTerm (is `List.snoc` j)
+                    ]
+                )
+            # Array.fold
+        , [ renderPointHandle p1 (HH.text "•") ]
+        , [ renderSpanHandle (Span { p0, p1 }) (HH.text ")") ]
+        ]
+          # Array.fold
+      )
+
+renderPointHandle :: Point -> HTML -> HTML
+renderPointHandle p label =
+  HH.div
+    [ HE.onClick (SetCursor_Action (PointCursor p) <<< Just) ]
+    [ label ]
+
+renderSpanHandle :: Span -> HTML -> HTML
+renderSpanHandle s label =
+  HH.div
+    [ HE.onClick (SetCursor_Action (SpanCursor s) <<< Just) ]
+    [ label ]
 
 --------------------------------------------------------------------------------
 -- State
@@ -97,23 +149,29 @@ parseUserActionsFromKeyboardEvent ke {} =
   shift = KeyboardEvent.shiftKey ke
   cmd = KeyboardEvent.ctrlKey ke || KeyboardEvent.metaKey ke
 
--- parseUserActionsFromMouseClick :: TODO -> State -> Maybe (Array UserAction)
--- parseUserActionsFromMouseClick = todo "parseUserActionsFromMouseClick" {}
-
 --------------------------------------------------------------------------------
 -- Action
 --------------------------------------------------------------------------------
 
-data Action =
-  UserAction_Action UserAction
+data Action
+  = UserAction_Action UserAction
+  | SetCursor_Action Cursor (Maybe MouseEvent)
 
 --------------------------------------------------------------------------------
 -- Logic
 --------------------------------------------------------------------------------
 
 handleUserAction :: UserAction -> H.HalogenM State Action Slots Output M Unit
-handleUserAction MoveLeft = todo "handleUserAction" {}
-handleUserAction MoveRight = todo "handleUserAction" {}
+handleUserAction MoveLeft = do
+  { term, cursor } <- get
+  case moveLeft_Cursor term cursor of
+    Nothing -> pure unit
+    Just cursor' -> modify_ _ { cursor = cursor' }
+handleUserAction MoveRight = do
+  { term, cursor } <- get
+  case moveRight_Cursor term cursor of
+    Nothing -> pure unit
+    Just cursor' -> modify_ _ { cursor = cursor' }
 handleUserAction SelectLeft = todo "handleUserAction" {}
 handleUserAction SelectRight = todo "handleUserAction" {}
 handleUserAction Delete = todo "handleUserAction" {}
--- src/Sexpze/Data/Sexp/Cursor.purs
+++ src/Sexpze/Data/Sexp/Cursor.purs
@@ -8,12 +8,28 @@ import Data.Array as Array
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe, fromMaybe')
 import Sexpze.Data.Sexp (Sexp(..))
-import Sexpze.Utility (bug)
+import Sexpze.Utility (bug, todo)
+
+--------------------------------------------------------------------------------
+-- Cursor
+--------------------------------------------------------------------------------
 
 data Cursor
   = PointCursor Point
   | SpanCursor Span
   | SelectCursor Select
+
+moveLeft_Cursor :: forall a. Sexp a -> Cursor -> Maybe Cursor
+moveLeft_Cursor x (PointCursor p) = PointCursor <$> moveLeft_Point x p
+moveLeft_Cursor _ _ = todo "moveLeft_Cursor" {}
+
+moveRight_Cursor :: forall a. Sexp a -> Cursor -> Maybe Cursor
+moveRight_Cursor x (PointCursor p) = PointCursor <$> moveRight_Point x p
+moveRight_Cursor _ _ = todo "moveRight_Cursor" {}
+
+--------------------------------------------------------------------------------
+-- Point
+--------------------------------------------------------------------------------
 
 -- | A `Point` is a position between two elements of a `Group`.
 data Point = Point (List Int) Int
@@ -24,34 +40,47 @@ topPoint = Point mempty 0
 consPoint :: Int -> Point -> Point
 consPoint i (Point is j) = Point (i : is) j
 
-movePoint_left :: forall a. Sexp a -> Point -> Maybe Point
-movePoint_left (Atom _) _ = empty
-movePoint_left (Group xs) (Point Nil j) =
+moveLeft_Point :: forall a. Sexp a -> Point -> Maybe Point
+moveLeft_Point (Atom _) _ = empty
+moveLeft_Point (Group xs) (Point Nil j) =
   if not (0 <= j && j <= Array.length xs) then bug "invalid Point tip"
   else if j == 0 then empty
   else pure (Point Nil (j - 1))
 
-movePoint_left (Group xs) (Point (i : is) j) =
+moveLeft_Point (Group xs) (Point (i : is) j) =
   let
     x = xs Array.!! i # fromMaybe' \_ -> bug "invalid Point step"
   in
-    consPoint i <$> movePoint_left x (Point is j)
+    consPoint i <$> moveLeft_Point x (Point is j)
 
-movePoint_right :: forall a. Sexp a -> Point -> Maybe Point
-movePoint_right (Atom _) _ = empty
-movePoint_right (Group xs) (Point Nil j) =
+moveRight_Point :: forall a. Sexp a -> Point -> Maybe Point
+moveRight_Point (Atom _) _ = empty
+moveRight_Point (Group xs) (Point Nil j) =
   if not (0 <= j && j <= Array.length xs) then bug "invalid Point tip"
   else if j == Array.length xs then empty
   else pure (Point Nil (j + 1))
 
-movePoint_right (Group xs) (Point (i : is) j) =
+moveRight_Point (Group xs) (Point (i : is) j) =
   let
     x = xs Array.!! i # fromMaybe' \_ -> bug "invalid Point step"
   in
-    consPoint i <$> movePoint_right x (Point is j)
+    consPoint i <$> moveRight_Point x (Point is j)
 
--- | A `Span` is a contiguous span between two `Point`s.
+--------------------------------------------------------------------------------
+-- Span
+--------------------------------------------------------------------------------
+
+-- | A `Span` is a contiguous span between two `Point`s that contains matching
+-- | numbers of opening and closing parentheses. A `Span` has an associated
+-- | number for how many outer unclosed parentheses it has.
 newtype Span = Span { p0 :: Point, p1 :: Point }
 
--- | A `Select` is composed of two `Span`s, with balanced groupings.
+--------------------------------------------------------------------------------
+-- Select
+--------------------------------------------------------------------------------
+
+-- | A `Select` is composed of two `Span`s that contains matching numbers of
+-- | opening and closing parentheses. A `Select` has an associated number for
+-- | how many outer unclosed parentheses it has.
 data Select = Select { s1 :: Span, s2 :: Span }
+
