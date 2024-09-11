aeeb7ff9880a3eedc84e7b936ee1b0d5fd04e319
Henry Blanchette <henry@monkeyface.com>
2024-09-11 12:49:17 -0400

    can pretty print failures in cursor tests now

--- test/Test/Cursor.purs
+++ test/Test/Cursor.purs
@@ -2,75 +2,89 @@ module Test.Cursor where
 
 import Prelude
 
+import Control.Monad.Error.Class (class MonadThrow, throwError)
 import Data.List (List(..), (:))
 import Data.List as List
 import Data.Newtype (wrap)
+import Effect.Exception (Error)
+import Effect.Exception as Exception
 import Sexpze.Data.Sexp (Sexp, Sexp'(..))
-import Sexpze.Data.Sexp.Cursor (Cursor(..), Point(..), SpanCursor(..), ZipperCursor(..), dragFromPoint)
+import Sexpze.Data.Sexp.Cursor (Cursor(..), Point(..), SpanCursor(..), ZipperCursor(..), dragFromPoint, prettySexpWithCursor)
 import Sexpze.Utility (todo)
-import Test.Spec (Spec, it)
+import Test.Spec (Spec)
 import Test.Spec as Spec
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions as Assertions
 
 spec :: Spec Unit
 spec = Spec.describe "Cursor" do
   spec_dragFromPoint
   pure unit
 
+shouldEqualCursor :: forall m a. MonadThrow Error m => Show a => Sexp a -> Cursor -> Cursor -> m Unit
+shouldEqualCursor xs c1 c2 = unless (c1 == c2) do
+  -- Assertions.fail (prettySexpWithCursor c1 xs <> " ≠ " <> prettySexpWithCursor c2 xs)
+  Assertions.fail (show c1 <> " ≠ " <> show c2)
+
 spec_dragFromPoint :: Spec Unit
 spec_dragFromPoint = Spec.describe "dragFromPoint" do
   Spec.describe "most basic" do
-    let
-      xs :: Sexp String
-      xs = [ Group [] ] :: Sexp String
-    it "don't move" do
-      shouldEqual
+    let xs = [ Group [] ] :: Sexp String
+    Spec.it "don't move" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 0) (point [] 0) xs)
         (InjectPoint $ point [] 0)
-    it "1-wide span" do
-      shouldEqual
+    Spec.it "1-wide span" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 0) (point [] 1) xs)
         (InjectSpanCursor $ spanCursor [] 0 1)
-    it "1-deep zipper" do
-      shouldEqual
+    Spec.it "1-deep zipper" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 0) (Point (wrap 0 : Nil) (wrap 0)) xs)
-        -- (InjectZipperCursor (ZipperCursor (spanCursor [] 0 1) (spanCursor [ 0 ] 0 0)))
         (InjectZipperCursor $ zipperCursor [] 0 1 [ 0 ] 0 0)
       pure unit
   Spec.describe "shallow" do
-    let
-      xs :: Sexp String
-      xs = [ Atom "a", Atom "b", Atom "c" ]
-    it "don't move" do
-      shouldEqual
+    let xs = [ atom "a", atom "b", atom "c" ]
+    Spec.it "don't move" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 0) (point [] 0) xs)
         (InjectPoint $ point [] 0)
       pure unit
-    it "1-wide span" do
-      shouldEqual
+    Spec.it "1-wide span" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 0) (point [] 1) xs)
         (InjectSpanCursor $ spanCursor [] 0 1)
-    it "2-wide span" do
-      shouldEqual
+    Spec.it "2-wide span" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 1) (Point Nil (wrap 3)) xs)
         (InjectSpanCursor $ spanCursor [] 1 3)
       pure unit
   Spec.describe "deeper" do
-    let xs = [ Atom "a", Group [ Atom "b", Atom "c" ], Atom "d" ] :: Sexp String
-    it "deeper span" do
-      shouldEqual
+    let xs = [ atom "a", Group [ atom "b", atom "c" ], atom "d" ]
+    Spec.it "deeper span" do
+      shouldEqualCursor xs
         (dragFromPoint (point [ 1 ] 0) (point [ 1 ] 1) xs)
         (InjectSpanCursor $ spanCursor [ 1 ] 0 1)
-    it "simplest zipper" do
-      shouldEqual
+    Spec.it "simplest zipper" do
+      shouldEqualCursor xs
         (dragFromPoint (point [] 1) (point [ 1 ] 0) xs)
-        (InjectZipperCursor $ zipperCursor [] 0 1 [ 1 ] 0 2)
+        (InjectZipperCursor $ zipperCursor [] 1 2 [ 1 ] 0 2)
     pure unit
   pure unit
 
 --------------------------------------------------------------------------------
 -- utilities
 --------------------------------------------------------------------------------
+
+newtype UnquotedString = UnquotedString String
+
+derive newtype instance Eq UnquotedString
+derive newtype instance Ord UnquotedString
+
+instance Show UnquotedString where
+  show (UnquotedString s) = s
+
+atom :: String -> Sexp' UnquotedString
+atom = Atom <<< UnquotedString
 
 point :: Array Int -> Int -> Point
 point is j = Point (is # map wrap # List.fromFoldable) (wrap j)
