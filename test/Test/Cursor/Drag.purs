f4fac664b9dc68b54eb8465f6c04676839c1e344
Henry Blanchette <henry@monkeyface.com>
2024-09-18 11:34:48 -0400

    using it_should_dragFromPointCursor

--- test/Test/Cursor/Drag.purs
+++ test/Test/Cursor/Drag.purs
@@ -48,13 +48,22 @@ spec = do
         e # it_should_dragFromPointCursor "end to start " (mkPointCursor [ 1 ] 1) (mkPointCursor [ 1 ] 0) (mkCursor [ 1 ] 0 2 [] 0 0 (Inner Start))
     describe "basic zippers" do
       let e = fromTreeToTermSpan [ L "a", B [ L "b", L "c", L "d" ], L "e" ]
-      e # it_should_dragFromPointCursor "outer start to inner start " (mkPointCursor [] 1) (mkPointCursor [ 1 ] 0) (mkCursor [] 1 1 [ 1 ] 0 0 (Inner Start))
-      e # it_should_dragFromPointCursor "outer start to inner start " (mkPointCursor [] 0) (mkPointCursor [ 1 ] 0) (mkCursor [] 0 1 [ 1 ] 0 0 (Inner Start))
-      e # it_should_dragFromPointCursor "inner start to outer start " (mkPointCursor [ 1 ] 0) (mkPointCursor [] 1) (mkCursor [] 1 1 [ 1 ] 0 3 (Outer Start))
-      e # it_should_dragFromPointCursor "inner end to far outer end " (mkPointCursor [ 1 ] 3) (mkPointCursor [] 2) (mkCursor [] 1 1 [ 1 ] 3 0 (Outer End))
-      e # it_should_dragFromPointCursor "inner end to outer end " (mkPointCursor [ 1 ] 3) (mkPointCursor [] 2) (mkCursor [] 1 1 [ 1 ] 3 0 (Outer End))
-      e # it_should_dragFromPointCursor "outer end to inner end " (mkPointCursor [] 2) (mkPointCursor [ 1 ] 3) (mkCursor [] 1 1 [ 1 ] 0 0 (Inner End))
-      e # it_should_dragFromPointCursor "far outer end to inner end " (mkPointCursor [] 3) (mkPointCursor [ 1 ] 3) (mkCursor [] 1 0 [ 1 ] 0 0 (Inner End))
+      do
+        e # it_should_dragFromPointCursor "outer start to inner start " (mkPointCursor [] 1) (mkPointCursor [ 1 ] 0) (mkCursor [] 1 1 [ 1 ] 0 0 (Inner Start))
+        e # it_should_dragFromPointCursor "outer start far to inner start " (mkPointCursor [] 0) (mkPointCursor [ 1 ] 0) (mkCursor [] 0 1 [ 1 ] 0 0 (Inner Start))
+        e # it_should_dragFromPointCursor "outer start to inner start far " (mkPointCursor [] 1) (mkPointCursor [ 1 ] 1) (mkCursor [] 1 1 [ 1 ] 1 0 (Inner Start))
+      do
+        e # it_should_dragFromPointCursor "inner start to outer start " (mkPointCursor [ 1 ] 0) (mkPointCursor [] 1) (mkCursor [] 1 1 [ 0 ] 0 3 (Outer Start))
+        e # it_should_dragFromPointCursor "inner start far to outer start " (mkPointCursor [ 1 ] 1) (mkPointCursor [] 1) (mkCursor [] 1 1 [ 0 ] 1 2 (Outer Start))
+        e # it_should_dragFromPointCursor "inner start to outer start far " (mkPointCursor [ 1 ] 0) (mkPointCursor [] 0) (mkCursor [] 0 1 [ 1 ] 0 3 (Outer Start))
+      do
+        e # it_should_dragFromPointCursor "inner end to outer end " (mkPointCursor [ 1 ] 3) (mkPointCursor [] 2) (mkCursor [] 1 1 [ 1 ] 3 0 (Outer End))
+        e # it_should_dragFromPointCursor "inner end to outer end far " (mkPointCursor [ 1 ] 3) (mkPointCursor [] 2) (mkCursor [] 1 1 [ 1 ] 3 0 (Outer End))
+        pending "inner end far to outer end "
+      do
+        e # it_should_dragFromPointCursor "outer end to inner end " (mkPointCursor [] 2) (mkPointCursor [ 1 ] 3) (mkCursor [] 1 1 [ 1 ] 0 0 (Inner End))
+        e # it_should_dragFromPointCursor "outer end far to inner end " (mkPointCursor [] 3) (mkPointCursor [ 1 ] 3) (mkCursor [] 1 0 [ 1 ] 0 0 (Inner End))
+        pending "outer end to inner end far "
   pure unit
 
 --------------------------------------------------------------------------------
@@ -79,13 +88,13 @@ should_dragFromCursor c1 p2 c e@(Span es) =
     unless (c' == c) do
       fail $ indent 2 $ "\n" <> Array.foldMap (_ <> "\n")
         [ "drag from\n"
-        , prettyTermWithCursor c1 (Sexp {} es)
+        , indent 4 $ prettyTermWithCursor c1 (Sexp {} es)
         , "\nto\n"
-        , prettyTermWithCursor (fromPointCursorToCursor p2 e) (Sexp {} es)
+        , indent 4 $ prettyTermWithCursor (fromPointCursorToCursor p2 e) (Sexp {} es)
         , "\nis supposed to be\n"
-        , prettyTermWithCursor c (Sexp {} es)
+        , indent 4 $ prettyTermWithCursor c (Sexp {} es)
         , "\nbut actually is\n"
-        , prettyTermWithCursor c' (Sexp {} es)
+        , indent 4 $ prettyTermWithCursor c' (Sexp {} es)
         ]
 
 --------------------------------------------------------------------------------
