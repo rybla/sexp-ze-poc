18ba8ae3ba9776c8b169cb36ae67dcd0cb1d4904
Henry Blanchette <henry@monkeyface.com>
2024-09-19 11:21:50 -0400

    checkpoint

--- src/Sexpze/Data/Sexp.purs
+++ src/Sexpze/Data/Sexp.purs
@@ -6,36 +6,38 @@ import Data.Array as Array
 import Data.Eq.Generic (genericEq)
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
+import Data.Tuple.Nested (type (/\), (/\))
 
 --------------------------------------------------------------------------------
 -- Sexp
 --------------------------------------------------------------------------------
 
 -- | Type parameters:
--- |   - `n` is data at each `Sexp`
--- |   - `a` is data at each `Atom`
-data Sexp node atom = Sexp node (Array (Sexp' node atom))
-data Sexp' node atom = Atom atom | Group (Sexp node atom)
+-- |   - `point` is data at point in a `Sexp`
+-- |   - `atom` is the data at each `Atom` in the `Sexp`
+newtype Sexp point atom = Sexp (Array (SexpItem point atom))
+type SexpItem point atom = point /\ Sexp' point atom
+data Sexp' point atom = Atom atom | Group (Sexp point atom)
 
-derive instance Generic (Sexp node atom) _
-derive instance Generic (Sexp' node atom) _
+derive instance Generic (Sexp point atom) _
+derive instance Generic (Sexp' point atom) _
 
-instance (Show node, Show atom) => Show (Sexp node atom) where
+instance (Show point, Show atom) => Show (Sexp point atom) where
   show x = genericShow x
 
-instance (Show node, Show atom) => Show (Sexp' node atom) where
+instance (Show point, Show atom) => Show (Sexp' point atom) where
   show x = genericShow x
 
-instance (Eq node, Eq atom) => Eq (Sexp node atom) where
+instance (Eq point, Eq atom) => Eq (Sexp point atom) where
   eq x = genericEq x
 
-instance (Eq node, Eq atom) => Eq (Sexp' node atom) where
+instance (Eq point, Eq atom) => Eq (Sexp' point atom) where
   eq x = genericEq x
 
-prettySexp :: forall node atom. Show node => Show atom => Sexp node atom -> String
-prettySexp (Sexp node sexp') = show node <> " " <> (sexp' # map prettySexp' # Array.intercalate " ")
+prettySexp :: forall point atom. Show point => Show atom => Sexp point atom -> String
+prettySexp (Sexp xs) = xs # map (\(_ /\ x) -> prettySexp' x) # Array.intercalate " "
 
-prettySexp' :: forall node atom. Show node => Show atom => Sexp' node atom -> String
+prettySexp' :: forall point atom. Show point => Show atom => Sexp' point atom -> String
 prettySexp' (Atom atom) = show atom
 prettySexp' (Group sexp) = prettySexp sexp
 
--- src/Sexpze/Data/Sexp/Cursor.purs
+++ src/Sexpze/Data/Sexp/Cursor.purs
@@ -10,86 +10,19 @@ import Data.Newtype (class Newtype, over2)
 import Data.Newtype as Newtype
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested (type (/\), (/\))
-import Sexpze.Data.Sexp (Sexp(..), Sexp')
+import Sexpze.Data.Sexp (Sexp(..), Sexp', SexpItem)
 import Sexpze.Utility (bug, todo)
 
---------------------------------------------------------------------------------
--- KidIndex
---------------------------------------------------------------------------------
+-- a cursor into a Sexp with an outer Path, an middle (selected) Path, and an
+-- inner Sexp
+data Cursor p a = Cursor (Path p a) (Path p a) (Sexp p a)
 
-newtype KidIndex = KidIndex Int
+-- a path into a Sexp to a sub-Sexp
+data Path p a = Path (List (NarrowTooth p a)) (WideTooth p a)
 
-derive instance Newtype KidIndex _
-derive newtype instance Show KidIndex
-derive newtype instance Eq KidIndex
-derive newtype instance Ord KidIndex
+-- a tooth around a sub-Sexp 
+data WideTooth p a = WideTooth (Array (SexpItem p a)) (Array (SexpItem p a))
 
-modifySexpAt :: forall n a. KidIndex -> (Sexp' n a -> Sexp' n a) -> Sexp n a -> Sexp n a
-modifySexpAt (KidIndex i) f (Sexp n e's) = Sexp n (e's # Array.modifyAt i f # fromMaybe' (\_ -> bug "modifySexpAt" "KidIndex out of bounds"))
-
-getSexpKid :: forall n a. KidIndex -> Sexp n a -> Sexp' n a
-getSexpKid (KidIndex i) (Sexp _ e's) = e's Array.!! i # fromMaybe' (\_ -> bug "getSexpKid" "KidIndex out of bounds")
-
-atKidIndex :: forall n a. KidIndex -> Sexp n a -> (Sexp' n a -> Sexp n a) /\ Sexp' n a
-atKidIndex i e =
-  Tuple
-    (\e' -> e # modifySexpAt i (const e'))
-    (e # getSexpKid i)
-
---------------------------------------------------------------------------------
--- PointIndex
---------------------------------------------------------------------------------
-
-newtype PointIndex = PointIndex Int
-
---------------------------------------------------------------------------------
--- Path
---------------------------------------------------------------------------------
-
-newtype Path = Path (List KidIndex)
-
-unconsPath :: Path -> Maybe (KidIndex /\ Path)
-unconsPath (Path Nil) = empty
-unconsPath (Path (i : is)) = pure (i /\ Path is)
-
-consPath :: KidIndex -> Path -> Path
-consPath i (Path is) = Path (i : is)
-
-instance Semigroup Path where
-  append (Path ph) (Path Nil) = Path ph
-  append (Path Nil) (Path ph) = Path ph
-  append (Path (i1 : Nil)) (Path (i2 : ph2)) = Path $ over2 KidIndex add i1 i2 : ph2
-  append (Path (i1 : ph1)) (Path ph2) = consPath i1 (Path ph1 <> Path ph2)
-
---------------------------------------------------------------------------------
--- PointCursor
---------------------------------------------------------------------------------
-
-data PointCursor = PointCursor Path PointIndex
-
---------------------------------------------------------------------------------
--- SpanCursor
---------------------------------------------------------------------------------
-
-data SpanCursor = SpanCursor Path PointCursor PointCursor
-
--- | safe constructor. checks:
--- |   - p1 <= p2
-spanCursor :: Path -> PointCursor -> PointCursor -> SpanCursor
-spanCursor = SpanCursor -- TODO
-
---------------------------------------------------------------------------------
--- ZipperCursor
---------------------------------------------------------------------------------
-
-data ZipperCursor = ZipperCursor SpanCursor SpanCursor
-
--- | safe constructor. checks:
--- |   - s2 is non-empty
-zipperCursor :: SpanCursor -> SpanCursor -> ZipperCursor
-zipperCursor = ZipperCursor -- TODO
-
---------------------------------------------------------------------------------
--- Cursor
---------------------------------------------------------------------------------
+-- a tooth around a Group
+data NarrowTooth p a = NarrowTooth (Array (SexpItem p a)) (Array (SexpItem p a))
 
